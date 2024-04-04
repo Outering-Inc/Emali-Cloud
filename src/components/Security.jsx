a00413184ecc1bd540649e75b1c9a7fa340e54c7
oyaro <hillaryoyaroh@gmail.com>
2024-04-04 16:52:13 +0300

    Update Security

--- src/components/Security.jsx
+++ src/components/Security.jsx
@@ -15,7 +15,7 @@ const Security = ({ backgroundClass }) =>  (
         Your customers' privacy is our<br className="sm:block hidden"/> first priority
       </h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-3`}>
-        Elloe is committed to the preservation, protection and due use of personal data.
+        Emali Cloud is committed to the preservation, protection and due use of personal data.
         Our platform is designed to ensure the privacy and security of end users while using our services. 
         As a company committed to the security of our client's personal data, we implement and maintain physical,
         administrative and technical controls to safeguard data while in transit and at rest.
