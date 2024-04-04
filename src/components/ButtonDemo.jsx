c65a718ffd80f50bd3859871633b296614251e64
oyaro <hillaryoyaroh@gmail.com>
2024-04-04 15:15:54 +0300

    Signup update

--- src/components/ButtonDemo.jsx
+++ src/components/ButtonDemo.jsx
@@ -3,7 +3,7 @@ import styles from '../style';
 
 const ButtonDemo = () =>  (
     <button type="button" className={` rounded-full px-6 py-2 font-poppins font-medium text-[16px] text-primary bg-orange-gradient rounded-[10px] outline-none opacity-100 hover:opacity-70`}>
-        Get Demo
+        Sign Up
     </button>
   );
 
