a01608019744232b5a5b77809b101472e022bbe0
oyaro <hillaryoyaroh@gmail.com>
2024-04-05 11:23:43 +0300

    --footer update--

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => (
       <div className="flex-[1] flex flex-col justify-start mr-10">
         <img
           src={logo}
-          alt="Elloe>AI"
+          alt="Emali Inc"
           className="w-[200px] h-[75.14px] object-contain"
         />
         <p className={`${styles.paragraph} mt-4 max-w-[312px]`}>
@@ -55,7 +55,7 @@ const Footer = () => (
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-        Copyright Ⓒ 2023 Elloe.AI. All Rights Reserved.
+        Copyright Ⓒ 2024 Emali Cloud. All Rights Reserved.
       </p>
       <div className="flex flex-row md:mt-0 mt-6">
         {extraFooterLinks.map((footer, index) => (
