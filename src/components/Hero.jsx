40cb717c14d5e0b0e3fa082d48e92ca42218e0a7
oyaro <hillaryoyaroh@gmail.com>
2024-04-12 09:51:52 +0300

    --update Hero section--

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,24 +14,30 @@ const Hero = () =>  (
         </div>
         <div className="flex flex-row justify-between items-center w-full">
         <h1 className="flex-1 font-poppins font-semibold ss:text-[60px] text-[20px] text-white ss:leading-[60px] leading-[15px]">
-          Your Next Cloud Gen<br className="sm:block hidden"/> {" "}
-            <span className="text-orange-gradient">Power Your Business</span> {" "}
+          Next Cloud <br className="sm:block hidden"/> {" "}
+            <span className="text-orange-gradient">Gen</span> {" "}
         </h1>
         <div className="ss:flex hidden md:mr-4 mr-0">
           <GetStarted/>
         </div>
         </div>
-        <h1 className="font-poppins font-semibold ss:text-[38px] text-[10px] text-white ss:leading-[60px] leading-[15px] w-full">instantly...</h1>
-        <p className={`${styles.paragraph} max-w-[450px] mt-3 `}>
-        "Transform your business with leading cloud solutions like AWS, Google Cloud and Azure. Embrace scalability, efficiency, and agility to drive innovation and growth. Seamlessly migrate to the cloud and unlock unparalleled potential for your organization."
+        <h1 className="font-poppins font-semibold ss:text-[38px] text-[10px] text-white ss:leading-[60px] leading-[15px] w-full">
+          Power Your Business Instantly...
+          </h1>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5 `}>
+          "Transform your business with leading cloud solutions like AWS, 
+          Google Cloud and Azure. Embrace scalability, efficiency, and agility to drive innovation and growth. 
+          Seamlessly migrate to the cloud and unlock unparalleled potential for your organization."
         </p>
       </div>
       <div className={`flex ${styles.flexCenter} md:my-0 my-10 relative `}>
         <img src={hero_left} alt="model" className="w-[50%] h-[50%] relative z-[5]"/> 
-        <img src={hero_right} alt="model" className="w-[65%] h-[65%] relative z-[5]"/>
+
+        {/**Gradient Start */}
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient"/>
         <div className="absolute z-[1] w-[80%] h-[80%] rounded-full buttom-40 white__gradient"/>
         <div className="absolute z-[0] w-[50%] h-[50%] right-20 buttom-20 blue__gradient"/>
+        {/*Gradinet End*/}
       </div>
       <div className={`ss:hidden ${styles.flexCenter}`}>
         <GetStarted/>
